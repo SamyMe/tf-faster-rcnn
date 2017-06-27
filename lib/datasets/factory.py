b192baa228f59e09bdcd553b999e97b1af67aafc
Samy Melaine <smelaine@mappy.priv>
2017-06-27 10:26:38 +0200

    cleaning lib/datasets

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -24,24 +24,6 @@ for split in ['train', 'test']:
     name = '{}_{}'.format('mappy', split)
     __sets[name] = (lambda split=split: mappy(split, mappy_devkit_path))
 
-# # Set up voc_<year>_<split> 
-# for year in ['2007', '2012']:
-  # for split in ['train', 'val', 'trainval', 'test']:
-    # name = 'voc_{}_{}'.format(year, split)
-    # __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
-# 
-# # Set up coco_2014_<split>
-# for year in ['2014']:
-  # for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
-    # name = 'coco_{}_{}'.format(year, split)
-    # __sets[name] = (lambda split=split, year=year: coco(split, year))
-# 
-# # Set up coco_2015_<split>
-# for year in ['2015']:
-  # for split in ['test', 'test-dev']:
-    # name = 'coco_{}_{}'.format(year, split)
-    # __sets[name] = (lambda split=split, year=year: coco(split, year))
-# 
 
 def get_imdb(name):
   """Get an imdb (image database) by name."""
